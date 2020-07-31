79696d29763803dbbf7d7890dd643952942029dc
Cooklord <imnoahcook@gmail.com>
2020-07-31 12:19:29 -1000

    Add span code to TraceExample

--- examples/trace/src/main/java/com/google/cloud/opentelemetry/example/trace/TraceExample.java
+++ examples/trace/src/main/java/com/google/cloud/opentelemetry/example/trace/TraceExample.java
@@ -2,7 +2,6 @@ package com.google.cloud.opentelemetry.example.trace;
 
 import com.google.cloud.opentelemetry.trace.TraceConfiguration;
 import com.google.cloud.opentelemetry.trace.TraceExporter;
-import com.google.cloud.trace.v2.TraceServiceClient;
 import io.opentelemetry.OpenTelemetry;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.trace.Span;
@@ -15,30 +14,28 @@ import java.io.IOException;
 public class TraceExample {
   private TraceExporter traceExporter;
 
-  // OTel API
   private Tracer tracer = OpenTelemetry.getTracer("io.opentelemetry.example.TraceExample");
 
   private void setupTraceExporter() {
-    
     // using default project ID
     TraceConfiguration configuration = TraceConfiguration.builder().setDeadline(Duration.ofMillis(30000)).build();
 
     try {
       this.traceExporter = TraceExporter.createWithConfiguration(configuration);
+
+      OpenTelemetrySdk.getTracerProvider()
+              .addSpanProcessor(SimpleSpanProcessor.newBuilder(this.traceExporter).build());
     } catch(IOException e) {
       System.out.println("Uncaught Excetption");
     }
 
-    OpenTelemetrySdk.getTracerProvider()
-            .addSpanProcessor(SimpleSpanProcessor.newBuilder(this.traceExporter).build());
-    System.out.println("Set up");
   }
 
   private void myUseCase() {
-    // Generate a span
-    Span span = this.tracer.spanBuilder("Start my wonderful use case").startSpan();
+    // generate a span
+    Span span = this.tracer.spanBuilder("Start my use case").startSpan();
     span.addEvent("Event 0");
-    // execute my use case - here we simulate a wait
+    // simulate work
     doWork();
     span.addEvent("Event 1");
     span.end();
@@ -55,11 +52,14 @@ public class TraceExample {
     TraceExample example = new TraceExample();
     example.setupTraceExporter();
 
-    try {
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {
+    while(true) {
+      example.myUseCase();
+      try {
+        // one hour
+        Thread.sleep(1000 * 60 * 60);
+      } catch (InterruptedException e) {
+      }
     }
-    System.out.println("Done");
 
   }
 }
