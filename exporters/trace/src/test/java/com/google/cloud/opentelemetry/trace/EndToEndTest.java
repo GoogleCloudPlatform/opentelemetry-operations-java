2710b76b66823371841a20d28bdd10c552741d8a
zoc <zoc@google.com>
2020-08-18 08:21:28 +1000

    Review changes wildcard import

--- exporters/trace/src/test/java/com/google/cloud/opentelemetry/trace/EndToEndTest.java
+++ exporters/trace/src/test/java/com/google/cloud/opentelemetry/trace/EndToEndTest.java
@@ -17,7 +17,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
