a77b28af2f41401d5281f72caf435ed4cc32aa1a
AlexanderMann <mannimal09@gmail.com>
2020-09-17 10:19:07 -0500

    Refactor: Leverage Google AutoService annotations, make null return into RunTimeException

--- exporters/auto/src/main/java/com/google/cloud/opentelemetry/auto/GoogleCloudSpanExporterFactory.java
+++ exporters/auto/src/main/java/com/google/cloud/opentelemetry/auto/GoogleCloudSpanExporterFactory.java
@@ -1,25 +1,20 @@
 package com.google.cloud.opentelemetry.auto;
 
+import com.google.auto.service.AutoService;
 import com.google.cloud.opentelemetry.trace.TraceExporter;
-import io.opentelemetry.javaagent.tooling.TracerInstaller;
 import io.opentelemetry.javaagent.tooling.exporter.ExporterConfig;
 import io.opentelemetry.javaagent.tooling.exporter.SpanExporterFactory;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
 import java.io.IOException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+@AutoService(SpanExporterFactory.class)
 public class GoogleCloudSpanExporterFactory implements SpanExporterFactory {
-
-    private static final Logger log = LoggerFactory.getLogger(TracerInstaller.class);
-
     @Override
     public SpanExporter fromConfig(ExporterConfig _config) {
         try {
             return TraceExporter.createWithDefaultConfiguration();
         } catch (IOException ex) {
-            log.error("Unable to create Google Trace exporter.", ex);
-            return null;
+            throw new RuntimeException(ex);
         }
     }
 }
