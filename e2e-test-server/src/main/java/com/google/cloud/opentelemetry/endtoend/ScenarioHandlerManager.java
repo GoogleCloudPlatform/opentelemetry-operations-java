57f2ad880af84a29168688c4fa205eb069f7f006
Josh Suereth <joshuasuereth@google.com>
2021-05-24 19:05:30 -0400

    Implement basicTrace integration test.

--- e2e-test-server/src/main/java/com/google/cloud/opentelemetry/endtoend/ScenarioHandlerManager.java
+++ e2e-test-server/src/main/java/com/google/cloud/opentelemetry/endtoend/ScenarioHandlerManager.java
@@ -15,8 +15,18 @@
  */
 package com.google.cloud.opentelemetry.endtoend;
 
+import com.google.cloud.opentelemetry.trace.TraceConfiguration;
+import com.google.cloud.opentelemetry.trace.TraceExporter;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import java.io.IOException;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * A container for all scenarios we handle.
@@ -28,6 +38,7 @@ public class ScenarioHandlerManager {
 
   public ScenarioHandlerManager() {
     register("health", this::health);
+    register("basicTrace", this::basicTrace);
   }
 
   /** Health check test. */
@@ -35,6 +46,23 @@ public class ScenarioHandlerManager {
     return Response.ok("");
   }
 
+  private Response basicTrace(Request request) {
+    return withTemporaryTracer(
+        (tracer) -> {
+          Span span =
+              tracer
+                  .spanBuilder("basicTrace")
+                  .setAttribute(Constants.TEST_ID, request.testId())
+                  .startSpan();
+          try {
+            return Response.ok("");
+          } finally {
+            span.end();
+          }
+        });
+  }
+
+  /** Default test scenario runner for unknown test cases. */
   private Response unimplemented(Request request) {
     return Response.invalidArugment("Unimplemented.");
   }
@@ -46,5 +74,39 @@ public class ScenarioHandlerManager {
   public Response handleScenario(String scenario, Request request) {
     ScenarioHandler handler = scenarios.getOrDefault(scenario, this::unimplemented);
     return handler.handle(request);
+  }
+
+  /**
+   * Helper to configure an OTel SDK exporting to cloud trace within the context of a function call.
+   */
+  private static <R> R withTemporaryTracer(Function<Tracer, R> handler) {
+    try {
+      OpenTelemetrySdk sdk = setupTraceExporter();
+      try {
+        Tracer tracer = sdk.getTracer(Constants.INSTRUMENTING_MODULE_NAME);
+        return handler.apply(tracer);
+      } finally {
+        sdk.getSdkTracerProvider().shutdown();
+      }
+    } catch (IOException e) {
+      // Lift checked exception into runtime to feed through lambda impls and make it out to
+      // test status.
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static OpenTelemetrySdk setupTraceExporter() throws IOException {
+    // Using default project ID and Credentials
+    TraceConfiguration configuration =
+        TraceConfiguration.builder().setDeadline(Duration.ofMillis(30000)).build();
+
+    TraceExporter traceExporter = TraceExporter.createWithConfiguration(configuration);
+    // Register the TraceExporter with OpenTelemetry
+    return OpenTelemetrySdk.builder()
+        .setTracerProvider(
+            SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(traceExporter).build())
+                .build())
+        .build();
   }
 }
