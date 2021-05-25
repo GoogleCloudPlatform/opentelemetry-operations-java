8943ecc6f0718b039e9c31206b430980fa7749b5
Josh Suereth <Joshua.Suereth@gmail.com>
2021-05-25 11:22:09 -0400

    Update e2e-test-server/src/main/java/com/google/cloud/opentelemetry/endtoend/ScenarioHandlerManager.java
    Co-authored-by: Aaron Abbott <aaronabbott@google.com>

--- e2e-test-server/src/main/java/com/google/cloud/opentelemetry/endtoend/ScenarioHandlerManager.java
+++ e2e-test-server/src/main/java/com/google/cloud/opentelemetry/endtoend/ScenarioHandlerManager.java
@@ -98,7 +98,7 @@ public class ScenarioHandlerManager {
     }
   }
 
-  /** Set up an OpenTelmetrySDk w/ export to google cloud. */
+  /** Set up an OpenTelemetrySDK w/ export to google cloud. */
   private static OpenTelemetrySdk setupTraceExporter() throws IOException {
     // Using default project ID and Credentials
     TraceConfiguration configuration =
