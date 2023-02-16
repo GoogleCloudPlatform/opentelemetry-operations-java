1dd732f05481d4d56db2d216819315e4347842c0
Pranav Sharma <sharmapranav@google.com>
2023-02-16 21:26:03 +0000

    Fix IllegalStateException when calling getProjectId()

--- exporters/trace/src/main/java/com/google/cloud/opentelemetry/trace/TraceConfiguration.java
+++ exporters/trace/src/main/java/com/google/cloud/opentelemetry/trace/TraceConfiguration.java
@@ -228,7 +228,13 @@ public abstract class TraceConfiguration {
      */
     public TraceConfiguration build() {
       // If project ID is not set, attempt to get the Default Project ID
-      if (Strings.isNullOrEmpty(getProjectId())) {
+      String currentProjectId;
+      try {
+        currentProjectId = getProjectId();
+      } catch (IllegalStateException e) {
+        currentProjectId = null;
+      }
+      if (Strings.isNullOrEmpty(currentProjectId)) {
         setProjectId(Strings.nullToEmpty(ServiceOptions.getDefaultProjectId()));
       }
       // Make a defensive copy of fixed attributes.
