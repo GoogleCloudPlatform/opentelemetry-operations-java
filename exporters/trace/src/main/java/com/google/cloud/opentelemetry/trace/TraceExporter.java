16fde69f6584131b71a295b4d60f7e69ae74dc16
Pranav Sharma <sharmapranav@google.com>
2023-02-17 17:55:26 +0000

    Run spotlessApply to fix style issues

--- exporters/trace/src/main/java/com/google/cloud/opentelemetry/trace/TraceExporter.java
+++ exporters/trace/src/main/java/com/google/cloud/opentelemetry/trace/TraceExporter.java
@@ -18,68 +18,69 @@ package com.google.cloud.opentelemetry.trace;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
-
-import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.concurrent.atomic.AtomicReference;
+import javax.annotation.Nonnull;
 
 public class TraceExporter implements SpanExporter {
 
-    private final TraceConfiguration.Builder customTraceConfigurationBuilder;
-    private final AtomicReference<SpanExporter> internalTraceExporter;
+  private final TraceConfiguration.Builder customTraceConfigurationBuilder;
+  private final AtomicReference<SpanExporter> internalTraceExporter;
 
-    private TraceExporter(TraceConfiguration.Builder configurationBuilder) {
-        this.customTraceConfigurationBuilder = configurationBuilder;
-        this.internalTraceExporter = new AtomicReference<>(null);
-    }
+  private TraceExporter(TraceConfiguration.Builder configurationBuilder) {
+    this.customTraceConfigurationBuilder = configurationBuilder;
+    this.internalTraceExporter = new AtomicReference<>(null);
+  }
 
-    private static SpanExporter generateStubTraceExporter(TraceConfiguration.Builder configBuilder) {
-        return new TraceExporter(configBuilder);
-    }
+  private static SpanExporter generateStubTraceExporter(TraceConfiguration.Builder configBuilder) {
+    return new TraceExporter(configBuilder);
+  }
 
-    private SpanExporter createActualTraceExporter() throws IOException {
-        return InternalTraceExporter.createWithConfiguration(this.customTraceConfigurationBuilder.build());
-    }
+  private SpanExporter createActualTraceExporter() throws IOException {
+    return InternalTraceExporter.createWithConfiguration(
+        this.customTraceConfigurationBuilder.build());
+  }
 
-    public static SpanExporter createWithDefaultConfiguration() {
-        return generateStubTraceExporter(TraceConfiguration.builder());
-    }
+  public static SpanExporter createWithDefaultConfiguration() {
+    return generateStubTraceExporter(TraceConfiguration.builder());
+  }
 
-    public static SpanExporter createWithConfiguration(TraceConfiguration.Builder configBuilder) {
-        return generateStubTraceExporter(configBuilder);
-    }
+  public static SpanExporter createWithConfiguration(TraceConfiguration.Builder configBuilder) {
+    return generateStubTraceExporter(configBuilder);
+  }
 
-    @Deprecated
-    public static SpanExporter createWithConfiguration(TraceConfiguration configuration) throws IOException {
-        return InternalTraceExporter.createWithConfiguration(configuration);
-    }
+  @Deprecated
+  public static SpanExporter createWithConfiguration(TraceConfiguration configuration)
+      throws IOException {
+    return InternalTraceExporter.createWithConfiguration(configuration);
+  }
 
-    @Override
-    public CompletableResultCode flush() {
-        if (internalTraceExporter.get() == null) {
-            return CompletableResultCode.ofFailure();
-        }
-        return internalTraceExporter.get().flush();
+  @Override
+  public CompletableResultCode flush() {
+    if (internalTraceExporter.get() == null) {
+      return CompletableResultCode.ofFailure();
     }
+    return internalTraceExporter.get().flush();
+  }
 
-    @Override
-    public CompletableResultCode export(@Nonnull Collection<SpanData> spanDataList) {
-        try {
-            internalTraceExporter.compareAndSet(null, createActualTraceExporter());
-            return internalTraceExporter.get().export(spanDataList);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return CompletableResultCode.ofFailure();
-        }
+  @Override
+  public CompletableResultCode export(@Nonnull Collection<SpanData> spanDataList) {
+    try {
+      internalTraceExporter.compareAndSet(null, createActualTraceExporter());
+      return internalTraceExporter.get().export(spanDataList);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return CompletableResultCode.ofFailure();
     }
+  }
 
-    @Override
-    public CompletableResultCode shutdown() {
-        if (internalTraceExporter.get() != null) {
-            return internalTraceExporter.get().shutdown();
-        } else {
-            return CompletableResultCode.ofFailure();
-        }
+  @Override
+  public CompletableResultCode shutdown() {
+    if (internalTraceExporter.get() != null) {
+      return internalTraceExporter.get().shutdown();
+    } else {
+      return CompletableResultCode.ofFailure();
     }
+  }
 }
