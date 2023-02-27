bdecba09e06a5b81e2a67061af994d321a8d475c
Pranav Sharma <sharmapranav@google.com>
2023-02-27 20:16:30 +0000

    Update javadoc for Noop exporter

--- exporters/trace/src/main/java/com/google/cloud/opentelemetry/trace/NoopSpanExporter.java
+++ exporters/trace/src/main/java/com/google/cloud/opentelemetry/trace/NoopSpanExporter.java
@@ -18,29 +18,43 @@ package com.google.cloud.opentelemetry.trace;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
-import java.util.Collection;
-import javax.annotation.Nonnull;
 
-/**
- * A noop implementation of a {@link SpanExporter}. This class is implemented as a singleton and
- * return a successful result on all public methods without performing any action.
- */
-class NoopSpanExporter implements SpanExporter {
+import javax.annotation.Nonnull;
+import java.util.Collection;
+
+/** A noop implementation of a {@link SpanExporter}. */
+final class NoopSpanExporter implements SpanExporter {
 
   NoopSpanExporter() {
     // prevent explicit public call to default constructor
   }
 
+  /**
+   * Noop implementation for exporting spans.
+   *
+   * @param spans The {@link Collection} of {@link SpanData} that need to be exported.
+   * @return a failure result code indicated via {@link CompletableResultCode#ofFailure()}.
+   */
   @Override
   public CompletableResultCode export(@Nonnull Collection<SpanData> spans) {
     return CompletableResultCode.ofFailure();
   }
 
+  /**
+   * Noop implementation for flushing current spans.
+   *
+   * @return a success result code indicated via {@link CompletableResultCode#ofSuccess()}.
+   */
   @Override
   public CompletableResultCode flush() {
     return CompletableResultCode.ofSuccess();
   }
 
+  /**
+   * Noop implementation for shutting down the current exporter.
+   *
+   * @return a success result code indicated via {@link CompletableResultCode#ofSuccess()}.
+   */
   @Override
   public CompletableResultCode shutdown() {
     return CompletableResultCode.ofSuccess();
