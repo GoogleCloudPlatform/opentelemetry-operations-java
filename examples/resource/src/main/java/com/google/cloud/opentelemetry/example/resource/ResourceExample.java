2dfd08f5b5b10b3c90139ba8bf760358e6fdc382
Pranav Sharma <sharmapranav@google.com>
2024-03-29 19:34:49 +0000

    Update resource example

--- examples/resource/src/main/java/com/google/cloud/opentelemetry/example/resource/ResourceExample.java
+++ examples/resource/src/main/java/com/google/cloud/opentelemetry/example/resource/ResourceExample.java
@@ -16,16 +16,34 @@
 package com.google.cloud.opentelemetry.example.resource;
 
 import io.opentelemetry.contrib.gcp.resource.GCPResourceProvider;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.ResourceConfiguration;
 import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
 
 public class ResourceExample {
   public static void main(String[] args) {
-    System.out.println("Detecting resource: Autoconfigure");
+    // Get the autoconfigured OpenTelemetry SDK
+    OpenTelemetrySdk openTelemetrySdk =
+        AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+
+    // Shows the resource attributes detected from the environment variables
+    // and system properties.
+    System.out.println("Detecting resource: Environment");
     Resource autoResource = ResourceConfiguration.createEnvironmentResource();
-    System.out.println(autoResource.getAttributes());
+    System.out.println(autoResource.getAttributes() + "\n");
+
+    // Shows the resource attributes detected by the GCP Resource Provider
     System.out.println("Detecting resource: hardcoded");
     GCPResourceProvider resourceProvider = new GCPResourceProvider();
-    System.out.println(resourceProvider.getAttributes());
+    System.out.println(resourceProvider.getAttributes() + "\n");
+
+    // Shows the attributes attached to the Resource that was set for TracerProvider
+    // via the autoconfiguration SPI.
+    // This works similarly for MeterProvider and LoggerProvider.
+    System.out.println("Detecting resource: Autoconfigure");
+    SdkTracerProvider autoConfTracerProvider = openTelemetrySdk.getSdkTracerProvider();
+    System.out.println(autoConfTracerProvider.toString() + "\n");
   }
 }
