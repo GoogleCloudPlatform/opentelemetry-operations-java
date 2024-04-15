efe0f69379eb030bb31ae93f71624b7369a08c79
Pranav Sharma <sharmapranav@google.com>
2024-04-15 04:14:02 +0000

    add additional mappings to enhance sample

--- examples/otlp-spring/src/main/java/com/google/cloud/opentelemetry/examples/otlpspring/ApplicationController.java
+++ examples/otlp-spring/src/main/java/com/google/cloud/opentelemetry/examples/otlpspring/ApplicationController.java
@@ -15,16 +15,49 @@
  */
 package com.google.cloud.opentelemetry.examples.otlpspring;
 
+import static com.google.cloud.opentelemetry.examples.otlpspring.Main.openTelemetrySdk;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
+import java.util.Optional;
+import java.util.Random;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApplicationController {
+  private static final String INSTRUMENTATION_SCOPE_NAME = ApplicationController.class.getName();
+  private static final Random random = new Random();
+
   private static final String INDEX_GREETING =
       "Welcome to OTLP Trace sample with Google Auth on Spring";
+  private static final String WORK_RESPONSE_FMT = "Work finished in %d ms";
 
   @GetMapping("/")
   public String index() {
     return INDEX_GREETING;
+  }
+
+  @GetMapping("/work")
+  public String simulateWork(@RequestParam(name = "desc") Optional<String> description) {
+    String desc = description.orElse("generic");
+    // Generate a span
+    Span span =
+        openTelemetrySdk.getTracer(INSTRUMENTATION_SCOPE_NAME).spanBuilder(desc).startSpan();
+    long workDurationMillis;
+    try (Scope scope = span.makeCurrent()) {
+      span.addEvent("Event A");
+      // Do some work for the use case
+      // Simulate work: this could be simulating a network request or an expensive disk operation
+      workDurationMillis = 100 + random.nextInt(5) * 100;
+      Thread.sleep(workDurationMillis);
+      span.addEvent("Event B");
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    } finally {
+      span.end();
+    }
+    return String.format(WORK_RESPONSE_FMT, workDurationMillis);
   }
 }
--- examples/otlp-spring/src/main/java/com/google/cloud/opentelemetry/examples/otlpspring/Main.java
+++ examples/otlp-spring/src/main/java/com/google/cloud/opentelemetry/examples/otlpspring/Main.java
@@ -15,13 +15,89 @@
  */
 package com.google.cloud.opentelemetry.examples.otlpspring;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporterBuilder;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporterBuilder;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.sdk.common.CompletableResultCode;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-  public static void main(String[] args) {
+
+  static OpenTelemetrySdk openTelemetrySdk;
+  private static GoogleCredentials googleCredentials;
+
+  public static void main(String[] args) throws IOException {
     System.out.println("Starting OTLP with Spring Boot and Google Auth");
+    googleCredentials = GoogleCredentials.getApplicationDefault();
+    openTelemetrySdk = setupOpenTelemetry();
     SpringApplication.run(Main.class, args);
+    flushBufferedTraces();
+  }
+
+  private static OpenTelemetrySdk setupOpenTelemetry() {
+    AutoConfiguredOpenTelemetrySdk autoConfOTelSdk =
+        AutoConfiguredOpenTelemetrySdk.builder()
+            .addSpanExporterCustomizer(
+                (exporter, configProperties) ->
+                    addAuthorizationHeaders(exporter, googleCredentials))
+            .build();
+    return autoConfOTelSdk.getOpenTelemetrySdk();
+  }
+
+  private static SpanExporter addAuthorizationHeaders(
+      SpanExporter exporter, GoogleCredentials credentials) {
+    Map<String, String> authHeaders = new ConcurrentHashMap<>();
+    authHeaders.put("Authorization", refreshToken(credentials));
+    if (exporter instanceof OtlpHttpSpanExporter) {
+      try {
+        credentials.refreshIfExpired();
+        OtlpHttpSpanExporterBuilder builder =
+            ((OtlpHttpSpanExporter) exporter).toBuilder().setHeaders(() -> authHeaders);
+        return builder.build();
+      } catch (IOException e) {
+        System.out.println("error:" + e.getMessage());
+        throw new RuntimeException(e);
+      }
+    } else if (exporter instanceof OtlpGrpcSpanExporter) {
+      try {
+        credentials.refreshIfExpired();
+        OtlpGrpcSpanExporterBuilder builder =
+            ((OtlpGrpcSpanExporter) exporter).toBuilder().setHeaders(() -> authHeaders);
+        return builder.build();
+      } catch (IOException e) {
+        System.out.println("error:" + e.getMessage());
+        throw new RuntimeException(e);
+      }
+    }
+    return exporter;
+  }
+
+  // Flush all buffered traces
+  private static void flushBufferedTraces() {
+    CompletableResultCode completableResultCode =
+        openTelemetrySdk.getSdkTracerProvider().shutdown();
+    // wait till export finishes
+    completableResultCode.join(10000, TimeUnit.MILLISECONDS);
+  }
+
+  private static String refreshToken(GoogleCredentials credentials) {
+    System.out.println("Refreshing Google Credentials");
+    try {
+      credentials.refreshIfExpired();
+      return String.format("Bearer %s", credentials.getAccessToken().getTokenValue());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
