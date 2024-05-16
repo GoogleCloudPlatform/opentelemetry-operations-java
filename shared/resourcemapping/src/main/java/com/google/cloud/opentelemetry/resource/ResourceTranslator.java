cf807deb4ba9d9b2b214040e84ac2908d1e90f7a
Pranav Sharma <sharmapranav@google.com>
2024-05-16 19:16:44 +0000

    Make private static variable final

--- shared/resourcemapping/src/main/java/com/google/cloud/opentelemetry/resource/ResourceTranslator.java
+++ shared/resourcemapping/src/main/java/com/google/cloud/opentelemetry/resource/ResourceTranslator.java
@@ -27,7 +27,7 @@ import java.util.Optional;
 /** Translates from OpenTelemetry Resource into Google Cloud's notion of resource. */
 public class ResourceTranslator {
 
-  private static String UNKNOWN_SERVICE_PREFIX = "unknown_service";
+  private final static String UNKNOWN_SERVICE_PREFIX = "unknown_service";
 
   private ResourceTranslator() {}
 
