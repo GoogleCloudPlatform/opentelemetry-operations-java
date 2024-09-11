2a30e85451d52217607ef4a159ac1187be593f3d
Pranav Sharma <sharmapranav@google.com>
2024-09-11 15:29:37 -0400

    Rename spring example (#367)

--- examples/spring-sleuth/src/main/java/com/google/cloud/opentelemetry/example/springsleuth/Main.java
+++ examples/spring-sleuth/src/main/java/com/google/cloud/opentelemetry/example/springsleuth/Main.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.cloud.opentelemetry.example.spring;
+package com.google.cloud.opentelemetry.example.springsleuth;
 
 import com.google.cloud.opentelemetry.trace.TraceExporter;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
