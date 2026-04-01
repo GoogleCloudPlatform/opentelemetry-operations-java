18f15e97d96e81b3930c87685a0ff474d6958df2
Pranav Sharma <sharmapranav@google.com>
2026-04-01 20:36:48 +0000

    Update Style

--- examples/autoinstrument-auth-extension/src/main/java/com/example/App.java
+++ examples/autoinstrument-auth-extension/src/main/java/com/example/App.java
@@ -1,14 +1,29 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class App {
 
@@ -39,16 +54,20 @@ public class App {
     @Override
     public void handle(HttpExchange t) throws IOException {
       String targetUrl = "https://httpbin.org/get";
-      Request request = new Request.Builder()
-          .url(targetUrl)
-          .build();
+      Request request = new Request.Builder().url(targetUrl).build();
 
       String responseBody = "";
       int responseCode = 200;
 
       try (Response response = client.newCall(request).execute()) {
         responseCode = response.code();
-        responseBody = "Outbound request to " + targetUrl + " returned: " + responseCode + "\nBody: " + response.body().string();
+        responseBody =
+            "Outbound request to "
+                + targetUrl
+                + " returned: "
+                + responseCode
+                + "\nBody: "
+                + response.body().string();
       } catch (IOException e) {
         responseCode = 500;
         responseBody = "Error making outbound request: " + e.getMessage();
